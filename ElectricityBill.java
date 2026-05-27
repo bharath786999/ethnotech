7b142f48778fec04e6debe7591a0ae69eb3ee40a
Bharath Kumar <bk2389535@gmail.com>
2026-05-27 10:55:07 +0530

    Delete ElectricityBill.java

--- ElectricityBill.java
+++
@@ -1,9 +0,0 @@
-class ElectricityBill{
-	public static void main(String[] args){
-		int units = 350;
-		System.out.println("Electricty bill : " + eleBill(units));
-	}
-	static int eleBill(int units){
-		return ((units > 250) & true) ? units - 250 : 0;
-	}
-}
